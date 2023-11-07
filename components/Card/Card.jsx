4562f8cb91060ef832c229401777a2feb4328a67
Shawn Almeida <shawn9920@gmail.com>
2023-11-08 04:20:58 +0530

    Fixed featured products

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,35 +1,38 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function Card(props) {
-    
-    return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            {/* <img className="w-full" src={props.img} alt="Sunset in the mountains"/> */}
-            <div className="w-full p-4">
-            <Image
-                src={props?.productImages[0]}
-                alt="featured Tattoo Sleeves"
-            //    width={100}
-            //    height={100}
-                fill
-                className='relative'
-            />
-            </div>
-            
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{props?.title}</div>
-                <p className="text-gray-700 text-base">
-                    {props?.description}
-                </p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-            </div>
-        </div>
-    )
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      {/* <img className="w-full" src={props.img} alt="Sunset in the mountains"/> */}
+      <div className="w-full p-4">
+        <Image
+          src={props?.productImages[0]?.secure_url}
+          alt="featured Tattoo Sleeves"
+          //    width={100}
+          //    height={100}
+          fill
+          className="relative"
+        />
+      </div>
+
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{props?.title}</div>
+        <p className="text-gray-700 text-base">{props?.description}</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #photography
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #travel
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          #winter
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-}
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dip2pjbju/image/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
